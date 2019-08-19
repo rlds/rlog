79c99d4ebca64af8b68c216b088860a4c72a16b8
吴道睿 <daorui.wu@tongdun.cn>
2019-08-19 14:01:56 +0800

    修复bug:仅移除日志文件夹下自己应用的日志

--- remove_log_file.go
+++ remove_log_file.go
@@ -7,85 +7,87 @@
 //
 package rlog
 
-import(
+import (
+	"fmt"
 	"io/ioutil"
+	"os"
+	"sort"
 	"strings"
 	"time"
-	"sort"
-	"os"
-	"fmt"
 )
 
 /*
-     删除n天前的日志文件
-     删除规则：删除最近日期 前推n天之前的日志
- */
+   删除n天前的日志文件
+   删除规则：删除最近日期 前推n天之前的日志
+*/
 
-func GetRemoveLogFileDayNum()int{
+func GetRemoveLogFileDayNum() int {
 	return removeLogFilePrevDay
 }
 
-func SetRemoveLogFileDayNum(n int)int{
+func SetRemoveLogFileDayNum(n int) int {
 	removeLogFilePrevDay = n
 	return removeLogFilePrevDay
 }
 
 type S_Files []os.FileInfo
 
-func (s S_Files) Len() int { return len(s) }
-func (s S_Files) Swap(i, j int){ s[i], s[j] = s[j], s[i] }
+func (s S_Files) Len() int           { return len(s) }
+func (s S_Files) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 func (s S_Files) Less(i, j int) bool { return s[i].ModTime().Unix() > s[j].ModTime().Unix() }
 
-func GetDirFiles(dir string)(files S_Files,err error){
-    files, err = ioutil.ReadDir(dir)
-    if err != nil {
-        return
-    }
-    sort.Sort(files)
-    return
+func GetDirFiles(dir string) (files S_Files, err error) {
+	files, err = ioutil.ReadDir(dir)
+	if err != nil {
+		return
+	}
+	sort.Sort(files)
+	return
 }
 
 // 获得需要删除的文件名列表
-func GetRemoveFiles(dirpath string,s S_Files,daynum int)(fpath []string){
-    if len(s) > 0 {
-        f1 := s[0].ModTime()
-        t := time.Date(f1.Year(), f1.Month(), f1.Day(), 0, 0, 0, 0, f1.Location())
-        t = t.AddDate( 0 , 0, -daynum )
-        utm := t.Unix()
-        for _,f := range s {
-            date := f.ModTime().Format("20060102")
-            fname := f.Name()
-            if strings.Contains(fname,date) { //仅清理包含日志日期标志的文件
-                if f.ModTime().Unix() < utm {
-                    fpath = append(fpath, dirpath +"/"+ fname )
-                }
-            }
-        }
-    }
-    return
+func GetRemoveFiles(dirpath, removeFilePrestr string, s S_Files, daynum int) (fpath []string) {
+	if len(s) > 0 {
+		f1 := s[0].ModTime()
+		t := time.Date(f1.Year(), f1.Month(), f1.Day(), 0, 0, 0, 0, f1.Location())
+		t = t.AddDate(0, 0, -daynum)
+		utm := t.Unix()
+		for _, f := range s {
+			date := f.ModTime().Format("20060102")
+			fname := f.Name()
+			if strings.Contains(fname, removeFilePrestr) && strings.Contains(fname, date) { //仅清理包含日志日期标志的文件
+				if f.ModTime().Unix() < utm {
+					fpath = append(fpath, dirpath+"/"+fname)
+				}
+			}
+		}
+	}
+	return
 }
 
-func removeLogFiles(dir string)( lasterr error){
+func removeLogFiles(dir string) (lasterr error) {
 	//主动设置 <=0 则表示不删除日志 手动清理
 	if removeLogFilePrevDay <= 0 {
 		return
 	}
-	
-	files , err := GetDirFiles(dir)
+
+	files, err := GetDirFiles(dir)
 	if err != nil {
 		lasterr = err
-		fmt.Println("getDirFileErr:",err.Error())
+		fmt.Println("getDirFileErr:", err.Error())
 		return
 	}
-	
-	rfiles := GetRemoveFiles(dir,files,removeLogFilePrevDay)
-	
-	for _,file := range rfiles {
-		err := os.Remove(file) 
+
+	removeFilePreStr := program + "."
+
+	rfiles := GetRemoveFiles(dir, removeFilePreStr, files, removeLogFilePrevDay)
+
+	for _, file := range rfiles {
+		err := os.Remove(file)
 		if err != nil {
 			lasterr = err
 			//删除日志文件错误
-			fmt.Println(file,"removeLogErr:",err.Error())
+			fmt.Println(file, "removeLogErr:", err.Error())
 		}
 	}
 	return
